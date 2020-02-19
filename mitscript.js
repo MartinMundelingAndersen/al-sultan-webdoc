b435a59cabbc8e370a9c10361a27fe805510f247
Sandra Froberg Birkefeldt <sandrafrobergbirkefeldt@Sandras-MBP.emnet.dk>
2020-02-19 20:34:57 +0100

    version 9 - 19022020

--- mitscript.js
+++ mitscript.js
@@ -1,1 +1,19 @@
 "use strict";
+
+//INTRO VIDEO
+/*var video = document.getElementById("video")
+
+document.getElementById("video").on('mouseenter', function () {
+    document.getElementById("video").get(0).play();
+});
+
+document.getElementById("video").on('mouseout', function () {
+    document.getElementById("video").get(0).pause();
+});*/
+
+var vid = document.getElementById("video");
+
+function enableLoop() {
+    vid.loop = true;
+    vid.load();
+}
