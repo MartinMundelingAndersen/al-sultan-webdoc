a15a4d49a4751677c3c6be8cb171856207e160b5
Sandra Froberg Birkefeldt <sandrafrobergbirkefeldt@Sandras-MBP.emnet.dk>
2020-02-21 09:24:17 +0100

    version 9 - 21022020

--- mitscript.js
+++ mitscript.js
@@ -1,19 +1,34 @@
 "use strict";
 
-//INTRO VIDEO
-/*var video = document.getElementById("video")
+//SLIDESHOW
+function gaaFremad() {
+    if (billedIndeks < billedliste.length - 1) {
+        billedIndeks++;
+    } else {
+        billedIndeks = 0;
+    }
 
-document.getElementById("video").on('mouseenter', function () {
-    document.getElementById("video").get(0).play();
-});
-
-document.getElementById("video").on('mouseout', function () {
-    document.getElementById("video").get(0).pause();
-});*/
-
-var vid = document.getElementById("video");
-
-function enableLoop() {
-    vid.loop = true;
-    vid.load();
+    document.getElementById("slidebillede").src = billedliste[billedIndeks];
 }
+
+function gaaTilbage() {
+    if (billedIndeks > 0) {
+        billedIndeks--;
+    } else {
+        billedIndeks = 2; //Kan ændres til billedeliste.lenght-1 i stedet for 2. Så kan man tilføje flere billeder til sit slideshow, uden at ændre særligt meget andet.
+    }
+
+    document.getElementById("slidebillede").src = billedliste[billedIndeks];
+}
+
+// ------- Hovedprogram ---------
+const billedliste = ["billed1.jpg", "loeve.jpg", "loevepar.jpg"];
+let billedIndeks = 0;
+
+document.getElementById("frem").addEventListener("click", function () {
+    gaaFremad();
+})
+
+document.getElementById("tilbage").addEventListener("click", function () {
+    gaaTilbage();
+})
